70272797f8e8d6400b7ee8580a08a738cf3a12c4
Juan Ignacio <jigomezit@gmail.com>
2023-04-17 16:43:22 -0300

    add counter

--- jig/js/scripts.js
+++ jig/js/scripts.js
@@ -32,3 +32,13 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+   let response = await fetch ("https://ixnykrwq63slstvci7tu6fieri0ozxif.lambda-url.us-east-1.on.aws/");
+   let data = await response.json();
+   counter.innerHTML = ` Views: ${data}`;
+}
+
+    updateCounter();
+
